686b0bef11afb3669d29edd0832c68a14ef24608
Denis Abdullin <denisabdullincz@gmail.com>
2023-05-08 10:52:14 +0200

    trend and forecast examples

--- narrator.R
+++ narrator.R
@@ -1,8 +1,39 @@
 library(shiny)
 library(narrator)
 library(dplyr)
+library(tidyr)
 library(bslib)
 library(shinyWidgets)
+library(prophet)
+library(DT)
+
+fit_prophet <- function(data) {
+  model <- suppressMessages(prophet(data))
+  future <- make_future_dataframe(model, periods = 12, freq = "month")
+  forecast <- predict(model, future)
+  return(forecast)
+}
+
+grouped_data <- sales %>%
+  dplyr::mutate(ds = lubridate::floor_date(Date, unit = "month")) %>%
+  dplyr::group_by(Region, ds) %>%
+  dplyr::summarise(y = sum(Sales, na.rm = TRUE)) %>%
+  tidyr::nest()
+
+grouped_data$forecast <- lapply(grouped_data$data, fit_prophet)
+
+actuals <- grouped_data %>%
+  dplyr::select(-forecast) %>%
+  unnest(data)
+
+forecast <- grouped_data %>%
+  dplyr::select(-data) %>%
+  unnest(forecast) %>%
+  dplyr::select(ds, yhat) %>%
+  dplyr::left_join(actuals) %>%
+  suppressMessages() %>%
+  dplyr::rename(Actuals = y,
+                Forecast = yhat)
 
 dimensions <- sales %>%
   select(-`Order ID`, -Promotion) %>%
@@ -55,7 +86,20 @@ ui <- fluidPage(
     ),
     column(
       width = 10,
-      narratorUI(id = "main")
+      tabsetPanel(
+        tabPanel(
+          title = "Descriptive",
+          narratorUI(id = "descriptive")
+        ),
+        tabPanel(
+          title = "Trend",
+          narratorUI(id = "trend")
+        ),
+        tabPanel(
+          title = "Forecast",
+          narratorUI(id = "forecast")
+        )
+      )
     )
   )
 )
@@ -70,8 +114,29 @@ server <- function(input, output, session) {
     req(input$measure, input$dimensions, input$summarization)
     
     narratorServer(
-      id = "main",
+      id = "descriptive",
+      narrative_type = "descriptive",
       df = reactive(sales),
+      format = TRUE,
+      summarization = input$summarization,
+      measure = input$measure,
+      dimensions = input$dimensions
+    )
+    
+    narratorServer(
+      id = "trend",
+      narrative_type = "trend",
+      df = reactive(sales),
+      format = TRUE,
+      summarization = input$summarization,
+      measure = input$measure,
+      dimensions = input$dimensions
+    )
+    
+    narratorServer(
+      id = "forecast",
+      narrative_type = "forecast",
+      df = reactive(forecast),
       format = TRUE,
       summarization = input$summarization,
       measure = input$measure,
